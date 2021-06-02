73e4977ccad68bc4272e02d99dd9c77c1850daf7
R0N1n-dev <mdrewj@live.com>
2021-06-02 18:36:28 +0300

    Addressed

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> ca9a44568efbbe42d4cf8bab6fd157d81729259c
 const drink_container = document.getElementById("drink");
 const search = document.querySelector(".search");
 
